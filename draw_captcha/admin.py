e4893c00772afc6f981acbbdca1741728bde460a
Henrik Heino <henrik.heino@gmail.com>
2017-10-06 00:05:24 +0300

    Admin list displays.

--- draw_captcha/admin.py
+++ draw_captcha/admin.py
@@ -7,7 +7,11 @@ from .models import Task, Picture
 class TaskAdmin(admin.ModelAdmin):
     readonly_fields = ['uuid', 'secret', 'created_at', 'task_type', 'adjective', 'noun', 'valid_pictures', 'invalid_pictures']
 
+    list_display =  ['__unicode__', 'secret', 'created_at', 'task_type', 'adjective', 'noun']
+
 
 @admin.register(Picture)
 class PictureAdmin(admin.ModelAdmin):
     readonly_fields = ['img', 'created_at', 'adjective', 'noun', 'valid_identifications', 'failed_identifications']
+
+    list_display =  ['__unicode__', 'created_at', 'adjective', 'noun', 'valid_identifications', 'failed_identifications']
